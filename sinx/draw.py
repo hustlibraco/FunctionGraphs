e1d6b29653a924fa158b1b4d87e45307c3f3e544
hustlibraco <hustlibraco@gmail.com>
2022-08-08 01:33:13 +0800

    命名标准化，绘图风格标准化

--- sinx/draw.py
+++ sinx/draw.py
@@ -26,9 +26,15 @@ plt.xticks([-2 * np.pi, -3 * np.pi / 2, -np.pi, -np.pi / 2, 0, np.pi / 2, np.pi,
            [r'-$2\pi$', r'-$3\pi/2$', r'-$\pi$', r'-$\pi/2$', '0', r'$\pi/2$', r'$\pi$', r'-$3\pi/2$', r'$2\pi$'])
 plt.yticks([-1, 1])
 ax.set_aspect(1)
-f1, = ax.plot(x, np.sin(x), label="f(x)")
-f2, = ax.plot(x, np.cos(x), label="f'(x)", linestyle="dashed")
-ax.legend(handles=[f1, f2], bbox_to_anchor=(1, 0), loc="upper right")
+
+# 函数和导数
+f1 = np.sin(x)
+f2 = np.cos(x)
+# 函数图像和导数图像
+g1, = ax.plot(x, f1, label="f(x)")
+g2, = ax.plot(x, f2, label="f'(x)", linestyle="dashed")
+
+ax.legend(handles=[g1, g2], bbox_to_anchor=(1, 0), loc="upper right")
 figpath = CURDIR.joinpath("sinx.png")
 fig.savefig(figpath, bbox_inches="tight")
 plt.show()
--- tanx/draw.py
+++ tanx/draw.py
@@ -27,15 +27,18 @@ plt.xticks([-2 * np.pi, -3 * np.pi / 2, -np.pi, -np.pi / 2, 0, np.pi / 2, np.pi,
            [r'-$2\pi$', r'-$3\pi/2$', r'-$\pi$', r'-$\pi/2$', '0', r'$\pi/2$', r'$\pi$', r'-$3\pi/2$', r'$2\pi$'])
 # plt.yticks([-1, 1])
 plt.ylim(-5, 5)
-# tanx表示
+ax.set_aspect(1)
+
+# 函数
 f1 = np.tan(x)
 # 移除渐近线
 f1[:-1][np.diff(f1) < 0] = np.nan
 # 导函数
 f2 = 1 / (np.cos(x) ** 2)
-
+# 函数和导数图像
 g1, = ax.plot(x, f1, label="f(x)")
 g2, = ax.plot(x, f2, label="f'(x)", linestyle="dashed")
+
 ax.legend(handles=[g1, g2], bbox_to_anchor=(1, 0), loc="upper right")
 figpath = CURDIR.joinpath("tanx.png")
 fig.savefig(figpath, bbox_inches="tight")
